e1ab84b0d7e389c781b2d6c51a2d2dc507f78057
bernardopire123 <169068301+bernardopire123@users.noreply.github.com>
2024-05-29 01:25:53 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("padrão feito para calcular o consumo eletrico");
+    System.out.println();
+    System.out.println("O valor da base considerado e");
+   double potencia = 200;
+    System.out.println(potencia);
+
+    double tempo = 5;
+    System.out.println(tempo);
+ 
+    double consumoEletrico = potencia * tempo;
+
+    System.out.println(consumoEletrico);
+  }
+}
